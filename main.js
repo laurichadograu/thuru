a2ead555bcd9b1a1b14acf37c5bc9b17397baccb
laurichadograu <126159388+laurichadograu@users.noreply.github.com>
2024-03-12 10:15:20 -0300

    gf

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const botoes = document.querySelectorAll(".botao");
 for (let i = 0; i < botoes.length;
     j++){
         botoes[i].onclick = function (){
+            for (let j = 0; j < botoes.length; j++){
+                botoes[j].classList.rempve(ativo);
+            }
             botoes[i].classList.add("ativo")
         }
     }
