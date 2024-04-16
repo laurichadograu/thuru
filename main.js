281ee758d97395c4e77494e5aebed98b2eedbcdb
laurichadograu <126159388+laurichadograu@users.noreply.github.com>
2024-04-16 11:05:53 -0300

    gf

--- main.js
+++ main.js
@@ -21,9 +21,12 @@ for (let i = 0; i < botoes.length; i++){
         let horas = Math.floor(minutos / 60);
         let dias = Math.floor(horas / 24);
 
-        segundos %= 60;
-        minutos %= 60;
-        horas %= 24;
-        return dias + "dias" + horas + "horas" + minutos "minutos" + segundos + "segundos";
+            segundos %= 60;
+            minutos %= 60;
+            horas %= 24;
+        return dias + "dias" 
+                + horas + "horas" 
+                + minutos "minutos" 
+                + segundos + "segundos";
         }
 
